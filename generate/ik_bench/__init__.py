3df7f4b0a9ad48e2f019a105d7ebcec56ec13f50
Stéphane Caron <stephane.caron@inria.fr>
2024-10-17 11:25:06 +0200

    WIP: Make scenarios list a dictionary

--- generate/ik_bench/__init__.py
+++ generate/ik_bench/__init__.py
@@ -4,10 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 Inria
 
+from .library import scenarios
 from .scenario import Scenario
 from .scene import Scene
 
 __all__ = [
     "Scenario",
     "Scene",
+    "scenarios",
 ]
