9af1f7abfec09eddedb4f406ab899937f3ab9f7e
Stéphane Caron <stephane.caron@inria.fr>
2024-12-02 09:22:23 +0100

    WIP: Switch from HDF5 to Parquet

--- ik_qpbenchmark.py
+++ ik_qpbenchmark.py
@@ -7,10 +7,8 @@
 import os
 from typing import Iterator
 
-import h5py
-import numpy as np
 import qpbenchmark
-from qpbenchmark import Problem
+from qpbenchmark import Problem, ProblemList
 from qpbenchmark.benchmark import main
 
 
@@ -19,7 +17,6 @@ class IkQpbenchmark(qpbenchmark.TestSet):
 
     @property
     def description(self) -> str:
-        """Description of the test set."""
         return (
             "Differential inverse kinematics QP problems "
             "generated from robot motions"
@@ -34,56 +31,16 @@ class IkQpbenchmark(qpbenchmark.TestSet):
         return False
 
     def __init__(self):
-        """Initialize test set."""
         super().__init__()
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.data_dir = os.path.join(current_dir, "data")
-        self.__add_known_solver_issues()
-        self.__add_known_solver_timeouts()
-
-    def __add_known_solver_issues(self):
-        """See how this is done in the maros_meszaros_qpbenchmark."""
-
-    def __add_known_solver_timeouts(self):
-        """See how this is done in the maros_meszaros_qpbenchmark."""
-
-    def yield_problems_from_file(self, file: h5py.File) -> Iterator[Problem]:
-        """Yield sequence of problems from a file.
-
-        Args:
-            file: The file object from which to yield the problems.
-
-        Yields:
-            Problem object read from file.
-        """
-        problems = file.get("problems")
-        if problems is None and "ik_Problem" in file:
-            problems = file["ik_Problem"]  # TODO: legacy
-        problem_names = list(problems.keys())
-        name_prefix = os.path.basename(file.filename).replace(".hdf5", "")
-        for problem in problem_names:
-            qp_data = {
-                "name": f"{name_prefix}_{problem}",
-            }
-            for data_name in problems[problem].keys():
-                if f"{data_name}/data" in problems[problem]:
-                    data = problems[f"{problem}/{data_name}/data"][:]
-                    data[data > 9e19] = +np.inf
-                    data[data < -9e19] = -np.inf
-                    qp_data[data_name] = data
-                else:
-                    qp_data[data_name] = None
-            qp_problem = Problem(**qp_data)
-            yield qp_problem
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.data_dir = os.path.join(script_dir, "data")
 
     def __iter__(self) -> Iterator[Problem]:
         """Iterate over test set problems."""
         for filename in os.listdir(self.data_dir):
-            if not filename.endswith(".hdf5"):
-                continue
-            filepath = os.path.join(self.data_dir, filename)
-            with h5py.File(filepath, "r") as file:
-                yield from self.yield_problems_from_file(file)
+            if filename.endswith(".parquet"):
+                filepath = os.path.join(self.data_dir, filename)
+                yield from ProblemList.yield_from_parquet(filepath)
 
 
 if __name__ == "__main__":
