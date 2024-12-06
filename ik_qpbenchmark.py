2fc45e93f720eb9a66ed5b8dbab0a52f76c48bcb
Stéphane Caron <stephane.caron@inria.fr>
2024-12-06 17:37:27 +0100

    Add readme to the data/ directory

--- ik_qpbenchmark.py
+++ ik_qpbenchmark.py
@@ -4,15 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 Inria
 
-import os
-from typing import Iterator
+from pathlib import Path
 
 import qpbenchmark
-from qpbenchmark import Problem, ProblemList
 from qpbenchmark.benchmark import main
 
 
-class IkQpbenchmark(qpbenchmark.TestSet):
+class IkQpbenchmark(qpbenchmark.ParquetTestSet):
     """IK test set."""
 
     @property
@@ -31,22 +29,12 @@ class IkQpbenchmark(qpbenchmark.TestSet):
         return False
 
     def __init__(self):
-        super().__init__()
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        self.data_dir = os.path.join(script_dir, "data")
-
-    def __iter__(self) -> Iterator[Problem]:
-        """Iterate over test set problems."""
-        for filename in os.listdir(self.data_dir):
-            if filename.endswith(".parquet"):
-                filepath = os.path.join(self.data_dir, filename)
-                yield from ProblemList.yield_from_parquet(filepath)
+        script_dir = Path(__file__).resolve().parent
+        parquet_path = script_dir / "data" / "ik_qpbenchmark.parquet"
+        super().__init__(parquet_path)
 
 
 if __name__ == "__main__":
-    test_set_path = os.path.abspath(__file__)
-    test_set_dir = os.path.dirname(test_set_path)
-    main(
-        test_set_path=test_set_path,
-        results_path=f"{test_set_dir}/results/qpbenchmark_results.csv",
-    )
+    test_set_path = Path(__file__).resolve()
+    results_path = test_set_path.parent / "results" / "qpbenchmark_results.csv"
+    main(test_set_path=test_set_path, results_path=results_path)
